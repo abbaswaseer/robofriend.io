4a54c7371789be9c0c3dabe8a98fa4a0a43b8f5e
Ghulam ababs <abbass.waseer@gmail.com>
2020-06-04 12:27:31 +0500

    Updates

---
+++ precache-manifest.a588f1e3a1cd62e91a8e7e0c8b9fcb1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdac2e4231d9de169a1ca6dce06e102b",
+    "url": "/robofriend.io/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriend.io/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c3e9ea61980e42e99c98",
+    "url": "/robofriend.io/static/css/main.afbfbb66.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriend.io/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend.io/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e9ea61980e42e99c98",
+    "url": "/robofriend.io/static/js/main.578a9d8c.chunk.js"
+  },
+  {
+    "revision": "7e71d31b656487bff240",
+    "url": "/robofriend.io/static/js/runtime-main.1bf9d62a.js"
+  }
+]);
